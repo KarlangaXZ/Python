90f946252f148ce8a6d5b1b0a92ccc727790af01
Carlos Linares <carlos.josewin@gmail.com>
2024-08-27 18:10:13 -0400

    first commit

---
+++ Alphabetic _order.py
@@ -0,0 +1,8 @@
+
+words = input("Ingrese una palabras: ")
+
+words = words.split()
+words.sort()
+
+for word in words:
+    print(word)
---
+++ Area of a Triangle.py
@@ -0,0 +1,5 @@
+num = int(input("Mostrar una tabla de multiplicacion para que numero?"))
+
+print("-------------------------------------------------------------")
+for x in range(1,13):
+        print(num,'x', x,'=',num * x)
